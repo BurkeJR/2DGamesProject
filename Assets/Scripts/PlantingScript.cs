6263f3263f750e2898347ccc366327787c3d5092
John Burke <burkejr19@gcc.edu>
2022-11-02 22:43:47 -0400

    enemy health adjustments

--- Assets/Scripts/PlantingScript.cs
+++ Assets/Scripts/PlantingScript.cs
@@ -11,7 +11,6 @@ public class PlantingScript : MonoBehaviour
     public DayNightScript _dnScript;
 
     public GameObject _player;
-    public Text _gameOver;
 
     public GameObject _CornPrefab;
     public GameObject _EggplantPrefab;
